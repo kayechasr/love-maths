cd399a7030fd79e138dd8ce348a9c25c72c6050e
Kaye <kayelawal@gmail.com>
2023-08-07 19:27:34 +0000

    display additional question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("You clicked submit");
       } else {
         let gameType = this.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     });
   }
+
+    runGame("addition");
 });
 
 //USE DOCSTRINGS /** */ (DocStrings are used - they are not regular comments. When you over over the function when its called you get the desription of that function - go hover over runGame)
@@ -22,10 +24,17 @@ document.addEventListener("DOMContentLoaded", function () {
  * the main game "loop", called when the script is first loaded
  * and after the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
   //creates random number between 1 and 25
   let num1 = Math.floor(Math.random() * 25 + 1);
-  let num2 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
 }
 
 runGame();
@@ -38,7 +47,11 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+}
 
 function displaySubtractQuestion() {}
 
